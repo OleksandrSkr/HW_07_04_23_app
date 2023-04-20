abe0121b67961cf54e6a728035a8e2eb50df0a98
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-04-20 20:42:36 +0300

    added app def

--- app.py
+++ app.py
@@ -2,14 +2,13 @@
 #import Person.Admin
 from Person.Person import Person
 
-john = Person("john", 23)
-johns_name = john.get_name()
-
-print(johns_name)
-from Person.Admin import Admin
+#from Person.Admin import Admin
 
 if __name__ == "__main__":
-    print(__name__)
-    print("hello word")
-    Person.Admin.Registration()
-
+#    john = Person("john", 23)
+#    johns_name = john.get_name()
+    Person.Registration(Person)
+#    print(__name__)
+#    print("hello word")
+#    Person.Admin.Registration()
+#    print(johns_name)
